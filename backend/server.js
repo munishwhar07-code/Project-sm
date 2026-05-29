7b61c4cbb809aad653f36cec3ed69badf58b2fb0
munishwhar07-code <munishwhar07@gmail.com>
2026-05-29 15:59:54 +0530

    modified server.js

--- backend/server.js
+++ backend/server.js
@@ -57,6 +57,6 @@ app.use(errorHandler);
 // Server
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+app.listen(PORT,"0.0.0.0", () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
